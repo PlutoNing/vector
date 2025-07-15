4d238fdc9d7bfa6e1cadcc6cd7d4614f65609567
paulning <1942681896@qq.com>
2025-07-16 01:13:16 +0800

    remove sink keep

--- src/sinks/mod.rs
+++ src/sinks/mod.rs
@@ -13,8 +13,6 @@ pub mod console;
 pub mod file;
 #[cfg(feature = "sinks-http")]
 pub mod http;
-#[cfg(feature = "sinks-keep")]
-pub mod keep;
 #[cfg(feature = "sinks-loki")]
 pub mod loki;
 #[cfg(feature = "sinks-mezmo")]
