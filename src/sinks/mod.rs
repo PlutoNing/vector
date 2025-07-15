0bccdb9d7acefc7fe2be442ef2adad9501facd66
paulning <1942681896@qq.com>
2025-07-16 01:16:40 +0800

    remove sink mezmo

--- src/sinks/mod.rs
+++ src/sinks/mod.rs
@@ -13,8 +13,6 @@ pub mod console;
 pub mod file;
 #[cfg(feature = "sinks-http")]
 pub mod http;
-#[cfg(feature = "sinks-mezmo")]
-pub mod mezmo;
 #[cfg(feature = "sinks-new_relic")]
 pub mod new_relic;
 #[cfg(feature = "sinks-webhdfs")]
