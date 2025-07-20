e6b39e5ba72623610e0b0a8b7aff98bbc7d515ca
paulning <1942681896@qq.com>
2025-07-21 02:33:51 +0800

    remove ConditionConfig

--- src/conditions/mod.rs
+++ src/conditions/mod.rs
@@ -1,5 +1,5 @@
 #![allow(missing_docs)]
-use vector_lib::configurable::configurable_component;
+
 
 use crate::event::Event;
 
@@ -60,46 +60,6 @@ impl Condition {
     }
 }
 
-/// An event matching condition.
-///
-/// Many methods exist for matching events, such as using a VRL expression, a Datadog Search query string,
-/// or hard-coded matchers like "must be a metric" or "fields A, B, and C must match these constraints".
-///
-/// They can specified with an enum-style notation:
-///
-/// ```toml
-/// condition.type = 'datadog_search'
-/// condition.source = 'NOT "foo"'
-/// ```
-#[configurable_component]
-#[derive(Clone, Debug)]
-#[serde(tag = "type", rename_all = "snake_case")]
-pub enum ConditionConfig {
-    /// Matches an event if it is a log.
-    #[configurable(metadata(docs::hidden))]
-    IsLog,
-
-    /// Matches an event if it is a metric.
-    #[configurable(metadata(docs::hidden))]
-    IsMetric,
-
-    /// Matches an event if it is a trace.
-    #[configurable(metadata(docs::hidden))]
-    IsTrace,
-}
-
-impl ConditionConfig {
-    pub fn build(
-        &self,
-        _enrichment_tables: &vector_lib::enrichment::TableRegistry,
-    ) -> crate::Result<Condition> {
-        match self {
-            ConditionConfig::IsLog => Ok(Condition::IsLog),
-            ConditionConfig::IsMetric => Ok(Condition::IsMetric),
-            ConditionConfig::IsTrace => Ok(Condition::IsTrace),
-        }
-    }
-}
 
 pub trait Conditional: std::fmt::Debug {
     /// Checks if a condition is true.
