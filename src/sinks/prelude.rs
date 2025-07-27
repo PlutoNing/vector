f73a1a15b7266dad4dd605e71904911355ad2765
paulning <1942681896@qq.com>
2025-07-27 21:49:23 +0800

    rm -rf src/sinks/util/encoding.rs

--- src/sinks/prelude.rs
+++ src/sinks/prelude.rs
@@ -30,7 +30,7 @@ pub use crate::{
     sinks::{
         util::{
 
-            encoding::{self, write_all},
+            encoding::{self},
             request_builder::{default_request_builder_concurrency_limit},
             retries::{RetryAction, RetryLogic},
 
