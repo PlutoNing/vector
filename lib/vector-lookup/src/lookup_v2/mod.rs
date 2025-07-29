96282322a01aa5e143fa222fb6629bd91df1de13
paulning <1942681896@qq.com>
2025-07-30 00:51:07 +0800

    rm -rf lib/vector-lookup/src/lookup_v2/optional_path.rs

--- lib/vector-lookup/src/lookup_v2/mod.rs
+++ lib/vector-lookup/src/lookup_v2/mod.rs
@@ -1,4 +1,3 @@
-mod optional_path;
 pub use vrl::path::{
     parse_target_path, parse_value_path, BorrowedSegment, OwnedSegment, OwnedTargetPath,
     OwnedValuePath, PathConcat, PathParseError, PathPrefix, TargetPath, ValuePath,
